f2588a6c12bdec07e7c1e73321fa1270d24bd95d
Emma <ekh12@ic.ac.uk>
2015-01-25 18:46:51 +0000

    following tutorial

--- LondonLoop/app/src/main/java/emma/londonloop/MainActivity.java
+++ LondonLoop/app/src/main/java/emma/londonloop/MainActivity.java
@@ -1,17 +1,40 @@
 package emma.londonloop;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    TextView mainTextView;
+    Button mainButton;
+    EditText mainEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener((android.view.View.OnClickListener) this);
+
+        // 3. Access the EditText defined in layout XML
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
     }
 
 
@@ -22,18 +45,4 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
